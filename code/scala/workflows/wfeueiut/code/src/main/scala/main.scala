e636df576d3e80ecf6ebe15d894c87b2ed97338d
b@prophecy.io <b@prophecy.io>
2021-10-11 19:35:11 +0000

    Workflow Code Updated

--- code/scala/workflows/wfeueiut/code/src/main/scala/main.scala
+++ code/scala/workflows/wfeueiut/code/src/main/scala/main.scala
@@ -15,12 +15,15 @@ object Main {
     val df_reformat_0_out0 = reformat_0(spark, df_source_0_out0)
     val df_set_operation_0_out0 =
       set_operation_0(spark, df_reformat_0_out0, df_source_0_out0)
-    val df_reformat_1_out0 = reformat_1(spark, df_set_operation_0_out0)
-    val df_source_1_out0   = source_1(spark)
+    val df_reformat_1_out0    = reformat_1(spark, df_set_operation_0_out0)
+    val df_source_1_out0      = source_1(spark)
+    val df_deduplicate_0_out0 = deduplicate_0(spark)
     df_reformat_1_out0.cache().count()
     df_reformat_1_out0.unpersist()
     df_source_1_out0.cache().count()
     df_source_1_out0.unpersist()
+    df_deduplicate_0_out0.cache().count()
+    df_deduplicate_0_out0.unpersist()
   }
 
   def main(args: Array[String]): Unit = {
