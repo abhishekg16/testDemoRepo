528fe35df64a024104644468e83831efdd73a77c
b@prophecy.io <b@prophecy.io>
2021-10-11 11:00:46 +0000

    Workflow Code Updated

---
+++ code/scala/workflows/fsfsdf/code/src/main/scala/graph/schema_transform_0.scala
@@ -0,0 +1,22 @@
+package graph
+
+import io.prophecy.libs._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import config.ConfigStore._
+import udfs.UDFs._
+import udfs._
+
+object schema_transform_0 {
+
+  def apply(spark: SparkSession, in: DataFrame): DataFrame =
+    in.drop("cmls_3ds_authntn_mthd")
+      .withColumn("new_col", col("cmls_aa_score_thrshld_excd"))
+      .withColumn(
+        "new_col",
+        struct(col("cmls_acct_ctry_cd_drvd"), lit("40").as("phoneNumber"))
+      )
+
+}
