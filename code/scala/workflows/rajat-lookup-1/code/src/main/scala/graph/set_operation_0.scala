6934cbc89b9d74ba77e7266d0938b114be919216
b@prophecy.io <b@prophecy.io>
2021-10-12 10:22:32 +0000

    Workflow Code Updated

--- code/scala/workflows/rajat-lookup-1/code/src/main/scala/graph/set_operation_0.scala
+++ code/scala/workflows/rajat-lookup-1/code/src/main/scala/graph/set_operation_0.scala
@@ -11,18 +11,7 @@ import udfs._
 
 object set_operation_0 {
 
-  def apply(spark: SparkSession, in0: DataFrame, in1: DataFrame): DataFrame = {
-    def unionWithSchema(
-      dataFrame:      DataFrame,
-      otherDataFrame: DataFrame
-    ): DataFrame =
-      dataFrame.union(
-        otherDataFrame
-          .select(dataFrame.columns.head, dataFrame.columns.tail: _*)
-      )
-    def unionAll(df: DataFrame*): DataFrame =
-      df.reduce((df1, df2) => unionWithSchema(df1, df2))
-    unionAll(in0, in1)
-  }
+  def apply(spark: SparkSession, in0: DataFrame, in1: DataFrame): DataFrame =
+    in0.except(in1)
 
 }
