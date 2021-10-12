9914c83cb53f7befb4a6677a9a252b3cdc48bf4a
b@prophecy.io <b@prophecy.io>
2021-10-12 20:48:56 +0000

    Workflow Code Updated

--- code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/graph/aggregate_0.scala
+++ code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/graph/aggregate_0.scala
@@ -12,15 +12,23 @@ import udfs._
 object aggregate_0 {
 
   def apply(spark: SparkSession, in: DataFrame): DataFrame =
-    in.agg(
-      sum(col("auth_aprv_cnt")).as("auth_aprv_cnt"),
-      sum(col("auth_tran_cnt")).as("auth_tran_cnt"),
-      sum(col("auth_us_aprv_amt"))
-        .cast(DecimalType(18, 2))
-        .as("auth_us_aprv_amt"),
-      sum(col("auth_us_tran_amt"))
-        .cast(DecimalType(18, 2))
-        .as("auth_us_tran_amt")
-    )
+    in.groupBy(
+        col("cpd_dt"),
+        col("cpd_tm"),
+        col("acct_num_regn_cd"),
+        col("prod_brnd_grp_cd"),
+        col("ntwrk_grp_cd"),
+        col("cmls_tran_seq_id_drvd")
+      )
+      .agg(
+        sum(col("auth_aprv_cnt")).as("auth_aprv_cnt"),
+        sum(col("auth_tran_cnt")).as("auth_tran_cnt"),
+        sum(col("auth_us_aprv_amt"))
+          .cast(DecimalType(18, 2))
+          .as("auth_us_aprv_amt"),
+        sum(col("auth_us_tran_amt"))
+          .cast(DecimalType(18, 2))
+          .as("auth_us_tran_amt")
+      )
 
 }
