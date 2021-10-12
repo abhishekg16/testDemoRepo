537a21e8a06fa5d51eb99304a4accbd3ea93e0e5
b@prophecy.io <b@prophecy.io>
2021-10-12 19:55:00 +0000

    Workflow Code Updated

--- code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/config/Config.scala
+++ code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/config/Config.scala
@@ -3,5 +3,9 @@ package config
 import config.ConfigStore._
 import io.prophecy.libs._
 
-case class Config(fabricName: String, CPD_DT: String, RUN_DATE: String)
-    extends ConfigBase
+case class Config(
+  fabricName:  String,
+  CPD_DT:      String,
+  RUN_DATE:    String,
+  TARGET_PATH: String
+) extends ConfigBase
