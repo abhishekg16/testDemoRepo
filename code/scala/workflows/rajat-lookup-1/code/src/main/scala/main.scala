b22aca9e47da35dbe9b08908492d6eb5c7cab4ac
b@prophecy.io <b@prophecy.io>
2021-10-12 08:41:34 +0000

    Workflow Code Updated

--- code/scala/workflows/rajat-lookup-1/code/src/main/scala/main.scala
+++ code/scala/workflows/rajat-lookup-1/code/src/main/scala/main.scala
@@ -11,12 +11,15 @@ import graph._
 object Main {
 
   def apply(spark: SparkSession): Unit = {
-    val df_source_0_out0   = source_0(spark)
+    val df_source_0_out0 = source_0(spark)
+    my_lookup_1(spark, df_source_0_out0)
     val df_reformat_0_out0 = reformat_0(spark, df_source_0_out0)
     val df_filter_0_out0   = filter_0(spark,   df_reformat_0_out0)
-    my_lookup_1(spark, df_source_0_out0)
+    val df_reformat_1_out0 = reformat_1(spark)
     df_filter_0_out0.cache().count()
     df_filter_0_out0.unpersist()
+    df_reformat_1_out0.cache().count()
+    df_reformat_1_out0.unpersist()
   }
 
   def main(args: Array[String]): Unit = {
