56314cf14ce52f9fb242b80a0280a1c2f58a9d87
b@prophecy.io <b@prophecy.io>
2021-10-12 20:00:43 +0000

    Workflow Code Updated

--- code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/graph/filter_credit.scala
+++ code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/graph/filter_credit.scala
@@ -12,6 +12,18 @@ import udfs._
 object filter_credit {
 
   def apply(spark: SparkSession, in: DataFrame): DataFrame =
-    in.filter(lit(true))
+    in.filter(
+      (col("cpd_dt") === lit(Config.CPD_DT))
+        .and(substring(col("cmls_tran_seq_id_drvd"), 4, 1) === lit("C"))
+        .and(col("CMLS_REQST_MSG_TYP").isin("0100", "0400"))
+        .and(
+          !col("cmls_acqr_pcr_drvd")
+            .isin("9088", "8088")
+            .and(
+              !col("cmls_vip_rsi_msg_catgy")
+                .isin("00155", "00156", "00159", "00094")
+            )
+        )
+    )
 
 }
