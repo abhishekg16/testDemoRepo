86de3ae3a08538db443ab87b42200ae91076150f
b@prophecy.io <b@prophecy.io>
2021-10-12 20:43:18 +0000

    Workflow Code Updated

--- code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/graph/sub_graph_hour/package.scala
+++ code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/graph/sub_graph_hour/package.scala
@@ -5,5 +5,9 @@ import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 package object sub_graph_hour {
-  def apply(spark: SparkSession, in0: DataFrame): Unit = {}
+
+  def apply(spark: SparkSession, in0: DataFrame): Unit = {
+    val df_reformat_0_out = reformat_0(spark)
+  }
+
 }
