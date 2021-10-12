0144c8ab06281f0979b9710347780d1814f2d29c
b@prophecy.io <b@prophecy.io>
2021-10-12 21:35:54 +0000

    Workflow Code Updated

--- code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/graph/sub_graph_1/package.scala
+++ code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/graph/sub_graph_1/package.scala
@@ -7,7 +7,8 @@ import org.apache.spark.sql.types._
 package object sub_graph_1 {
 
   def apply(spark: SparkSession, in0: DataFrame): Unit = {
-    val df_reformat_0_out = reformat_0(spark, in0)
+    val df_reformat_0_out  = reformat_0(spark, in0)
+    val df_aggregate_0_out = aggregate_0(spark)
   }
 
 }
