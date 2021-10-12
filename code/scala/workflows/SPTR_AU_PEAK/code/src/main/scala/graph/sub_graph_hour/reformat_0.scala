b157649696284470772db0494fc69573877569e6
b@prophecy.io <b@prophecy.io>
2021-10-12 20:50:37 +0000

    Workflow Code Updated

--- code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/graph/sub_graph_hour/reformat_0.scala
+++ code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/graph/sub_graph_hour/reformat_0.scala
@@ -10,5 +10,19 @@ import udfs.UDFs._
 import udfs._
 
 object reformat_0 {
-  def apply(spark: SparkSession, in: DataFrame): DataFrame = in
+
+  def apply(spark: SparkSession, in: DataFrame): DataFrame =
+    in.select(
+      col("cpd_dt"),
+      concat(substring(col("cpd_tm"), 1, 2), lit(":00:00")).as("cpd_tm"),
+      col("acct_num_regn_cd"),
+      col("prod_brnd_grp_cd"),
+      col("ntwrk_grp_cd"),
+      col("cmls_tran_seq_id_drvd"),
+      col("auth_aprv_cnt"),
+      col("auth_tran_cnt"),
+      col("auth_us_aprv_amt"),
+      col("auth_us_tran_amt")
+    )
+
 }
