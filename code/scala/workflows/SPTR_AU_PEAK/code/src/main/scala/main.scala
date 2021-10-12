9ae4fa58e0dc6ed1796249f590ffa1170f6e2781
b@prophecy.io <b@prophecy.io>
2021-10-12 19:38:21 +0000

    Workflow Code Updated

--- code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/main.scala
+++ code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/main.scala
@@ -9,9 +9,12 @@ import udfs._
 import graph._
 
 object Main {
-  def apply(spark: SparkSession): Unit = {}
 
-  def main(args:   Array[String]): Unit = {
+  def apply(spark: SparkSession): Unit = {
+    val df_source_0_out = source_0(spark)
+  }
+
+  def main(args: Array[String]): Unit = {
     import config._
     ConfigStore.Config = ConfigurationFactoryImpl.fromCLI(args)
     val spark: SparkSession = SparkSession
