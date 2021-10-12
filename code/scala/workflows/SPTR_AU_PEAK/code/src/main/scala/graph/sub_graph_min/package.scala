87dd39a5eb55917d516d28ac2b2f70e38408fbef
b@prophecy.io <b@prophecy.io>
2021-10-12 20:57:17 +0000

    Workflow Code Updated

--- code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/graph/sub_graph_min/package.scala
+++ code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/graph/sub_graph_min/package.scala
@@ -7,8 +7,7 @@ import org.apache.spark.sql.types._
 package object sub_graph_min {
 
   def apply(spark: SparkSession, in0: DataFrame): Unit = {
-    val df_reformat_1_out = reformat_1(spark)
-    val df_reformat_0_out = reformat_0(spark)
+    val df_reformat_0_out = reformat_0(spark, in0)
   }
 
 }
