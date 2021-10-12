c2f945db196e44137e89564e0fb17d62945555a1
b@prophecy.io <b@prophecy.io>
2021-10-12 20:55:43 +0000

    Workflow Code Updated

--- code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/graph/sub_graph_min/package.scala
+++ code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/graph/sub_graph_min/package.scala
@@ -4,6 +4,6 @@ import org.apache.spark._
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
-package object sub_graph_1 {
+package object sub_graph_min {
   def apply(spark: SparkSession, in0: DataFrame): Unit = {}
 }
--- code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/main.scala
+++ code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/main.scala
@@ -8,7 +8,7 @@ import udfs.UDFs._
 import udfs._
 import graph._
 import graph.sub_graph_hour
-import graph.sub_graph_1
+import graph.sub_graph_min
 
 object Main {
 
@@ -29,7 +29,7 @@ object Main {
     val df_reformat_1_out      = reformat_1(spark,           df_join_0_out)
     val df_aggregate_0_out     = aggregate_0(spark,          df_reformat_1_out)
     val df_sub_graph_hour_out0 = sub_graph_hour.apply(spark, df_aggregate_0_out)
-    val df_sub_graph_1_out0    = sub_graph_1.apply(spark,    df_aggregate_0_out)
+    val df_sub_graph_min_out0  = sub_graph_min.apply(spark,  df_aggregate_0_out)
   }
 
   def main(args: Array[String]): Unit = {
