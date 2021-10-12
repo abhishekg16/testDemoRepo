180e52f5a2297d487e68f9a4d100f797a2d331f5
b@prophecy.io <b@prophecy.io>
2021-10-12 20:20:35 +0000

    Workflow Code Updated

--- code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/graph/filter_1.scala
+++ code/scala/workflows/SPTR_AU_PEAK/code/src/main/scala/graph/filter_1.scala
@@ -12,6 +12,21 @@ import udfs._
 object filter_1 {
 
   def apply(spark: SparkSession, in: DataFrame): DataFrame =
-    in.filter(lit(true))
+    in.filter(
+      col("CMLS_REQST_MSG_TYP_DRVD")
+        .isin("0100", "0200")
+        .or(
+          (col("CMLS_REQST_MSG_TYP_DRVD") === lit("0220"))
+            .and(col("CMLS_POSCOND_CD") === lit("06"))
+        )
+        .and(col("CMLS_ISO_TRAN_AMT_USD") > lit(0))
+        .and(
+          lpad(col("CMLS_PROC_TRAN_CD"), 2,    "0")
+            .isin("00",                  "01", "10", "11", "40", "50", "21")
+            .and(
+              (col("CMLS_CPD_TM") =!= lit(1)).or(col("CMLS_VIC") =!= lit("OCB"))
+            )
+        )
+    )
 
 }
