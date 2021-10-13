c1013033ca956a210dea9d97037cd1894cf939ac
b@prophecy.io <b@prophecy.io>
2021-10-13 05:44:52 +0000

    Workflow Code Updated

--- code/scala/workflows/W2/code/src/main/scala/main.scala
+++ code/scala/workflows/W2/code/src/main/scala/main.scala
@@ -11,8 +11,8 @@ import graph._
 object Main {
 
   def apply(spark: SparkSession): Unit = {
-    val df_source_0_out = source_0(spark)
     val df_source_1_out = source_1(spark)
+    val df_source_0_out = source_0(spark)
   }
 
   def main(args: Array[String]): Unit = {
